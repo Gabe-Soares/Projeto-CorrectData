33d41427e63202dffd977ceb9a5f9406a6ba78f6
Gabriel Soares <gabriel.soares.a@hotmail.com>
2019-01-18 22:38:54 -0200

    Querys de população do banco inseridas no Script de criação.

--- SQL/Script de criação.sql
+++ SQL/Script de criação.sql
@@ -1,3 +1,4 @@
+/* Definição e estruturação do banco de dados e da tabela. */
 CREATE DATABASE CorrectData_Contatos;
 
 USE CorrectData_Contatos;
@@ -12,3 +13,17 @@ CREATE TABLE CONTATOS_TB(
 	tel2_contato VARCHAR(15),
 	tel2_tipo VARCHAR(11)
 );
+
+/* INSERT de população para testes no desenvolvimento da aplicação WEB. */
+INSERT INTO dbo.CONTATOS_TB(nome_contato, nasc_data, email_contato, email_tipo, tel1_contato, tel1_tipo) values
+('Alexandre Barroso', '20001015', 'ale_barroso@gmail.com', 'Pessoal', '11 99999-9999', 'Celular'),
+('Bruno Lopes', '20000823', 'brunolopes@gmail.com', 'Profissional', '41 98888-8888', 'Celular'),
+('Ivan Messias', '19990911', 'ivan123@gmail.com', 'Pessoal', '11 97777-7777', 'Celular'),
+('Marcos dos Santos', '19980402', 'msantos321@gmail.com', 'Pessoal', '11 96666-6666', 'Celular');
+
+ /* INSERT de registro com campos nulos para teste. */
+INSERT INTO dbo.CONTATOS_TB(nome_contato, nasc_data, email_contato, email_tipo, tel1_contato, tel1_tipo) values
+('Gabriel Moura', '19950101', 'g1m2@gmail.com', 'Pessoal', NULL, NULL);
+
+/* SELECT para garantir a execução dos INSERTs. */
+SELECT * FROM CONTATOS_TB;
